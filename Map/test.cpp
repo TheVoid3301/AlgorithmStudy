2e3edfa14de52ff189883aee76e851a316df0918
thevoid3301 <1449812768@qq.com>
2024-12-24 23:32:41 +0800

    测试

--- Map/test.cpp
+++ Map/test.cpp
@@ -1,17 +1,11 @@
 #include<iostream>
 #include<cmath>
+#include <algorithm>
 using namespace std;
 typedef long long ll;
-const int N=1e4;
-const ll X=343720;
-const ll Y=233333;
 int main(){
-    for(ll x=2;x<=N;x+=2){
-        for(ll y=2;y<=N;y+=2){
-            if (15*Y*y==17*X*x){
-                cout << x << "  " << y;
-                return 0;
-            }
-        }
-    }
+    ll a =11686480;
+    ll b = 10311600;
+    ll c = 12375982320;
+    cout << c % a << "   " << c % b;
 }
