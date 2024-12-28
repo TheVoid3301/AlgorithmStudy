de224bf4fe096d24f71c186fdc232035606ddbc3
thevoid3301 <1449812768@qq.com>
2024-12-29 00:41:19 +0800

    中等题修改

--- 19704.团建.cpp
+++ 19704.团建.cpp
@@ -6,36 +6,49 @@ int c[N],d[N];
 map<int,vector<int>>mp1;
 map<int,vector<int>>mp2;
 int maxx = 0;
-void dfs(int x,int y,int cnt){
-    if(c[x]!=d[x]){
-        return;
+void dfs(int x,int y,int cnt)
+{
+  if(c[x]!=d[y])
+  {
+    return;
+  }
+  maxx = max(cnt+1,maxx);
+  int s1 = mp1[x].size();
+  int s2 = mp2[y].size();
+  for(int i = 0;i<s1;i++)
+  {
+    for(int j = 0;j<s2;j++)
+    {
+      dfs(mp1[x][i],mp2[y][j],cnt+1);
     }
-    maxx = max(cnt+1,maxx);
-    int s1 = mp1[x].size();
-    int s2 = mp2[y].size();
-    for(int i = 0;i<s1;i++){
-        for(int j = 0;j<s2;j++){
-            dfs(mp1[x][i],mp2[y][j],cnt+1);
-            
-        }
-    }
-
+  }
 }
-int main(){
-    cin>>n>>m;
-    for(int i = 1;i<=n;i++) cin>>c[i];
-    for(int i = 1;i<=m;i++) cin>>d[i];
-    for(int i = 1;i<=n-1;i++){
-        int a,b;
-        cin>>a>>b;
-        mp1[a].push_back(b);
+int main()
+{
+  cin>>n>>m;
+  for(int i = 1;i<=n;i++) cin>>c[i];
+  for(int i = 1;i<=m;i++) cin>>d[i];
+  for(int i = 1;i<=n-1;i++)
+  {
+    int a,b;
+    cin>>a>>b;
+    if(a > b)
+    {
+      swap(a, b);
     }
-    for(int i = 1;i<=m-1;i++){
-        int a,b;
-        cin>>a>>b;
-        mp2[a].push_back(b);
+    mp1[a].push_back(b);
+  }
+  for(int i = 1;i<=m-1;i++)
+  {
+    int a,b;
+    cin>>a>>b;
+    if(a > b)
+    {
+      swap(a, b);
     }
-    dfs(1,1,0);
-    cout<<maxx<<endl;
-    return 0;
+    mp2[a].push_back(b);
+  }
+  dfs(1,1,0);
+  cout<<maxx<<endl;
+  return 0;
 }
