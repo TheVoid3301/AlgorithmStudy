13c1c5e76b8ff9f072728287dae85acb2090451b
thevoid3301 <1449812768@qq.com>
2025-01-07 22:15:53 +0800

    dp修改

--- 216.地宫取宝.cpp
+++ 216.地宫取宝.cpp
@@ -1,45 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int n, m, k;
-int a[55][55];//输入所给数组值所分配的内存空间
-int dp[55][55][15][15];//开创记忆化的存储空间
-//因为只进行向下走和向右走，所有写成这个样子，不明白的可以在了解以下笛卡尔积，向下是x轴，向右是y轴(一般情况下)
-int dx[] = {1,0};
-int dy[] = {0,1};
-int p = 1e9+7;//索取余数
-//dfs()里面写x,y,mx,cnt分别代表更新的坐标，最大值，宝物数目
-long long dfs(int x, int y, int mx, int cnt)
+const int N = 55, MOD = 1000000007;
+ 
+int w[N][N], n, m, k;
+int f[N][N][13][14];    
+ 
+int main()
 {
-  if(x == n && y == m)  return cnt == k;//这个是结束临界条件，到最后结束跳出
-  if(dp[x][y][mx][cnt] != -1)   return  dp[x][y][mx][cnt];//进行记忆化搜索的地方，如果之前保存过这个值就不用再次计算而是直接使用保存过的值(值不会因为后续深搜改变的改变而是不变)
-  long long res = 0;//后续进行记忆化存储的给予赋值
-  for(int i = 0; i < 2; i++){
-  //进行向下或者向右的搜索
-      int nx = x + dx[i];
-      int ny = y + dy[i];
-      if(nx < 1 || ny < 1 || nx > n || ny > m)  continue;
-  //拿这个东西
-      if(a[nx][ny] > mx && cnt < k) res = (res + dfs(nx, ny, a[nx][ny], cnt + 1)) % p;
-  //不拿这个东西
-      res = (res + dfs(nx, ny, mx, cnt)) % p;
-  }
-  //进行记忆化搜索的重要部分
-  return dp[x][y][mx][cnt] = res;
-}
-int main(){
-   //将dp数组置为-1，不置为0是因为宝物价值可能为0出现异议
-    memset(dp, -1, sizeof dp);
     cin >> n >> m >> k;
-    for(int i = 1;i <= n; i++)
-    {        
-      for(int j = 1; j <= m; j++)
-      {
-        cin >> a[i][j];
-        a[i][j]++;
-   //这里的++是为了让宝物价值最小也为1，问题问的是路线情况，并不用担心价值问题
-      }
-    }
-   //对一开始的宝物拿和不拿都进行深搜获得最终答案         
-    cout << (dfs(1, 1, 0, 0) + dfs(1, 1, a[1][1], 1)) % p << endl;
+    for (int i = 1; i <= n; i ++)
+        for (int j = 1; j <= m; j ++) cin >> w[i][j], w[i][j] ++;
+    
+    // 初始化
+    f[1][1][1][w[1][1]] = 1;  // 取
+    f[1][1][0][0] = 1;        // 不取
+    
+    for (int i = 1; i <= n; i ++)
+        for (int j = 1; j <= m; j ++)
+        {
+            if (i == 1 && j == 1) continue; // 初始话的跳过
+            for (int u = 0; u <= k; u ++)
+                for (int v = 0; v <= 13; v ++)
+                {
+                    f[i][j][u][v] = (f[i][j][u][v] + f[i][j - 1][u][v]) % MOD;  // 状态计算 1
+                    f[i][j][u][v] = (f[i][j][u][v] + f[i - 1][j][u][v]) % MOD;  // 状态计算 2
+                    if (u > 0 && w[i][j] == v)    // u > 0 加不加都行, 不影响答案, 因为 u == 0的时候表示什么都没选, 进入下面的循环也没意义
+                    {
+                        for (int c = 0; c < v; c ++)  // 常见问题解释的就是这里, 需要加上比 v 小的f, 才能让 f[i][j][k][c]表示的含义正确
+                        {
+                            f[i][j][u][v] = (f[i][j][u][v] + f[i][j - 1][u - 1][c]) % MOD; // 状态计算 3
+                            f[i][j][u][v] = (f[i][j][u][v] + f[i - 1][j][u - 1][c]) % MOD; // 状态计算 4
+                        }
+                    }
+                }
+        }
+    int res = 0;
+    for (int i = 0; i <= 13; i ++) res = (res + f[n][m][k][i]) % MOD;
+    cout << res << endl;
     return 0;
 }
