63da80f06deaf2871e984898ec467471c10e8294
thevoid3301 <1449812768@qq.com>
2025-01-19 00:23:53 +0800

    79，完美解决，注意dfs的形参，有些一定要引用，不然就是超时，推测是每次递归新建变量的时间太多了

--- 79.单词搜索.cpp
+++ 79.单词搜索.cpp
@@ -7,38 +7,43 @@
 // @lc code=start
 class Solution {
 public:
-    vector<pair<int, int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    bool check(vector<vector<char>>& board, vector<vector<int>>& visited, int i, int j, string& s, int k) {
-        if (board[i][j] != s[k]) {
+    int a[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    bool dfs(vector<vector<char>>& board, vector<vector<int>>& visited, int x, int y, string& word, int p)
+    {
+        if(board[x][y] != word[p])
+        {
             return false;
-        } else if (k == s.length() - 1) {
+        }
+        else if(board[x][y] == word[p] && p == word.length() - 1)
+        {
             return true;
         }
-        visited[i][j] = true;
+        visited[x][y] = 1;
         bool result = false;
-        for (const auto& dir: directions) {
-            int newi = i + dir.first, newj = j + dir.second;
-            if (newi >= 0 && newi < board.size() && newj >= 0 && newj < board[0].size()) {
-                if (!visited[newi][newj]) {
-                    bool flag = check(board, visited, newi, newj, s, k + 1);
-                    if (flag) {
-                        result = true;
-                        break;
-                    }
+        for(int i = 0; i < 4; ++i)
+        {
+            int nx = x + a[i][0], ny = y + a[i][1];
+            if(nx >= 0 && nx < board.size() && ny >= 0 && ny < board[0].size() && visited[nx][ny] == 0)
+            {
+                bool flag = dfs(board, visited, nx, ny, word, p + 1);
+                if(flag)
+                {
+                    result = true;
+                    break;
                 }
             }
         }
-        visited[i][j] = false;
+        visited[x][y] = 0;
         return result;
     }
-
     bool exist(vector<vector<char>>& board, string word) {
-        int h = board.size(), w = board[0].size();
-        vector<vector<int>> visited(h, vector<int>(w));
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                bool flag = check(board, visited, i, j, word, 0);
-                if (flag) {
+        vector<vector<int>> visited(board.size(), vector<int>(board[0].size(), 0));
+        for(int i = 0; i < board.size(); ++i)
+        {
+            for(int j = 0; j < board[0].size(); ++j)
+            {
+                if(dfs(board, visited, i, j, word, 0))
+                {
                     return true;
                 }
             }
