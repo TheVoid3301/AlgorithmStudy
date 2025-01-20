041dd034b03bca004eea3be76479b82eebe7432e
thevoid3301 <1449812768@qq.com>
2025-01-20 23:51:29 +0800

    答案正确，但是需要优化

---
+++ P1217.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool check (string s)
+{
+    for (int i = 0; i < s.size() / 2; ++i)
+    {
+        if (s[i] != s[s.size() - 1 - i])
+        {
+            return false; 
+        }
+    }
+    return true;
+}
+bool checksu (int k)
+{
+    for(int i = 2; i <= k / 2; ++i)
+    {
+        if (k % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main ()
+{
+    int a, b;
+    string s;
+    cin >> a >> b;
+    for (int i = a; i <= b; ++i)
+    {
+        if (i % 2 == 0)
+        {
+            continue;
+        }
+        s = to_string(i);
+        if (check(s))
+        {
+            if (checksu(i))
+            {
+                cout << s << endl;
+            }
+        }
+    }
+    return 0;
+}
