f579076247080923fef5c531dc6df33eea5faf2f
thevoid3301 <1449812768@qq.com>
2025-01-24 00:10:35 +0800

    轻松

--- P2550.cpp
+++ P2550.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main ()
 {
-
+    int n, t;
+    vector<bool> lotto(34, false);
+    vector<int> win(7, 0);
+    cin >> n;
+    for (int i = 0 ; i < 7; ++i)
+    {
+        cin >> t;
+        lotto[t] = true;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        int k = 0;
+        for (int j = 0; j < 7; ++j)
+        {
+            cin >> t;
+            if (lotto[t])
+            {
+                ++k;
+            }
+        }
+        ++win[7 - k];
+    }
+    for (int i = 0; i <= 6; ++i)
+    {
+        cout << win[i] << " ";
+    }
     return 0;
 }
