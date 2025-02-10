5c8ab33aa8d7eab2e5bda7418a45323fd90c1c0d
thevoid3301 <1449812768@qq.com>
2025-02-10 21:42:57 +0800

    ease

---
+++ 189.轮转数组.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=cpp
+ *
+ * [189] 轮转数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> newArr(n);
+        for (int i = 0; i < n; ++i) {
+            newArr[(i + k) % n] = nums[i];
+        }
+        nums.assign(newArr.begin(), newArr.end());
+    }
+};
+// @lc code=end
+
