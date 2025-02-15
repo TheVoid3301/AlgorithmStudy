2846aefc884d2de0ce07b99fc382a03ae00887c2
thevoid3301 <1449812768@qq.com>
2025-02-15 15:35:24 +0800

    java写的简单题，只能说java的语法不仅臃肿还难用

---
+++ 383.赎金信.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=383 lang=cpp
+ *
+ * [383] 赎金信
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        int[] cnt = new int[26];
+        for (char c : magazine.toCharArray()) {
+            cnt[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            cnt[c - 'a']--;
+            if(cnt[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
