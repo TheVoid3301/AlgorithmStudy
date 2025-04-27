9dc9cf9d1737fcc272fed624b630f0645bfb0aef
thevoid3301 <1449812768@qq.com>
2025-04-28 00:11:40 +0800

    滑动窗口

---
+++ 2302.统计得分小于-k-的子数组数目.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=2302 lang=cpp
+ *
+ * [2302] 统计得分小于 K 的子数组数目
+ */
+
+// @lc code=start
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        int n = nums.size();
+        long long res = 0, total = 0;
+        for (int i = 0, j = 0; j < n; j++) {
+            total += nums[j];
+            while (i <= j && total * (j - i + 1) >= k) {
+                total -= nums[i];
+                i++;
+            }
+            res += j - i + 1;
+        }
+        return res;
+    }
+};
+// @lc code=end
+
