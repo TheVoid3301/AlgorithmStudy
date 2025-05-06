8dd8a2221318843e2c2b7e5529da26b205cf9f3b
thevoid3301 <1449812768@qq.com>
2025-05-06 21:51:31 +0800

    easy

---
+++ 3356.零数组变换-ii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=3356 lang=cpp
+ *
+ * [3356] 零数组变换 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        
+    }
+};
+// @lc code=end
+
