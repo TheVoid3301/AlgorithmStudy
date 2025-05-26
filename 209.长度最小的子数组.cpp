11414551d2bb4a90b19eab5388cf728a75e14d6d
thevoid3301 <1449812768@qq.com>
2025-05-27 00:03:05 +0800

    前缀和加二分查找

---
+++ 209.长度最小的子数组.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=cpp
+ *
+ * [209] 长度最小的子数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        int ans = INT_MAX;
+        vector<int> sums(n + 1, 0); 
+        // 为了方便计算，令 size = n + 1 
+        // sums[0] = 0 意味着前 0 个元素的前缀和为 0
+        // sums[1] = A[0] 前 1 个元素的前缀和为 A[0]
+        // 以此类推
+        for (int i = 1; i <= n; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+        for (int i = 1; i <= n; i++) {
+            int target = s + sums[i - 1];
+            auto bound = lower_bound(sums.begin(), sums.end(), target);
+            if (bound != sums.end()) {
+                ans = min(ans, static_cast<int>((bound - sums.begin()) - (i - 1)));
+            }
+        }
+        return ans == INT_MAX ? 0 : ans;
+    }
+};
+// @lc code=end
+
