89ec018afb922c27aad21b5e220db16c0c2d37d1
thevoid3301 <1449812768@qq.com>
2025-06-06 00:22:07 +0800

    easy

---
+++ LCR 123. 图书整理 I.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> reverseBookList(ListNode* head) {
+        vector<int> res;
+        while (head) {
+            res.insert(res.begin(), head->val);
+            head = head->next;
+        }
+        return res;
+    }
+};
