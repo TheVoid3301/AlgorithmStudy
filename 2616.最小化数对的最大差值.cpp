755cdb33e19db22b94c666b94153421a39ee5d9d
thevoid3301 <1449812768@qq.com>
2025-06-13 21:48:37 +0800

    二分答案加贪心

---
+++ 2616.最小化数对的最大差值.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=2616 lang=cpp
+ *
+ * [2616] 最小化数对的最大差值
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        sort(nums.begin(), nums.end());
+        auto check = [&](int mx) -> bool {
+            int cnt = 0;
+            for (int i = 0; i < nums.size() - 1; i++) {
+                if (nums[i + 1] - nums[i] <= mx) {
+                    cnt++;
+                    i++;
+                }
+            }
+            return cnt >= p;
+        };
+        int left = 0, right = nums.back() - nums[0];
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (check(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
+
+// @lc code=end
+
