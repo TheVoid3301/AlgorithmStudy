12ccd3d4007848bd732bcaa43fea43d92e90ad11
thevoid3301 <1449812768@qq.com>
2025-06-29 00:19:22 +0800

    easy

---
+++ 1307.口算难题.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1307 lang=cpp
+ *
+ * [1307] 口算难题
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isSolvable(vector<string>& words, string result) {
+        
+    }
+};
+// @lc code=end
+
