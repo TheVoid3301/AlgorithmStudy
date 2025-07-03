5e8c050aa73771443c70108c4cf9ff6085d9cd65
thevoid3301 <1449812768@qq.com>
2025-07-04 01:32:56 +0800

    easy

---
+++ 120.三角形最小路径和.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=120 lang=cpp
+ *
+ * [120] 三角形最小路径和
+ */
+
+// @lc code=start
+#include <vector>
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> f(n, vector<int>(n));
+        f[0][0] = triangle[0][0];
+        for (int i = 1; i < n; ++i) {
+            f[i][0] = f[i - 1][0] + triangle[i][0];
+            for (int j = 1; j < i; ++j) {
+                f[i][j] = min(f[i - 1][j - 1], f[i - 1][j]) + triangle[i][j];
+            }
+            f[i][i] = f[i - 1][i - 1] + triangle[i][i];
+        }
+        return *min_element(f[n - 1].begin(), f[n - 1].end());
+    }
+};
+// @lc code=end
+
