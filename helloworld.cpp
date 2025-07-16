f77c5cfb8eb5139f30060c4b37cc6f856e9f7cd0
thevoid3301 <1449812768@qq.com>
2025-07-16 23:08:54 +0800

    easy

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,60 +5,9 @@
 
 using namespace std;
 
-struct Datetime {
-    int year, month, day, hour, min, second;
-};
-
-time_t getTimestamp(int year, int month, int day, int hour, int min, int sec) {
-    struct tm tm = {};
-    tm.tm_year = year - 1900; // struct tm 内部会自动帮你加上一个 1900, 因此要手动减去
-    tm.tm_mon = month - 1; // struct tm 内部会自动帮你加上一个 1, 因此要手动减去
-    tm.tm_mday = day;
-    tm.tm_hour = hour;
-    tm.tm_min = min;
-    tm.tm_sec = sec;
-    //return timegm(&tm); // 使用UTC时间转换
-    return _mkgmtime(&tm);
-}
-
-void solve() {
-  // 1.难点一：要注意样例给的数据是 两个字符串(中间有个空格) + 一个 int
-  // 我一开始以为只是一个字符串
-    string dateStr, timeStr;
-    int x;
-    cin >> dateStr >> timeStr >> x;
-    string datetimeStr = dateStr + " " + timeStr;
-
-  // 2.难点二: 将获取的时间字符串提取时间信息
-    Datetime dt;
-    sscanf(datetimeStr.c_str(), "%d-%d-%d %d:%d:%d",
-           &dt.year, &dt.month, &dt.day, &dt.hour, &dt.min, &dt.second);
-
-
-  // 3.难点三: 获取并处理时间戳
-  // 这里如果不不以时间戳为思路处理，就要暴力的从1970年1月1日 00:00:00开始计算！！相比之下前者更好  
-    time_t timestamp = getTimestamp(dt.year, dt.month, dt.day, dt.hour, dt.min, dt.second);
-    time_t interval = x * 60;// 因为题目闹钟的时间单位是min, 而时间戳是以秒为单位, 因此需要将闹钟时间间隔转换为以秒为单位
-    time_t remainder = timestamp % interval;  // 除去前面 interval 的倍数时间，剩下的就是可以往前推移的时间
-    time_t recent = timestamp - remainder;    // 当前时间戳-可以推移的时间 = 最近一次响铃的时间
-
-    struct tm *tm = gmtime(&recent); // 用于将 time_t 类型的时间戳（通常表示自 Unix 纪元即1970年1月1日 00:00:00 UTC 以来的秒数）转换为 struct tm 结构体
-  // 就这一步，就帮我省去了超级多的麻烦！如果我自己处理时间戳，需要处理很多时间进位的操作！易错且麻烦
-
-
-    printf("%04d-%02d-%02d %02d:%02d:%02d\n",
-           tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
-           tm->tm_hour, tm->tm_min, tm->tm_sec);
-}
-
-
 int main() {
-    std::cout << "dasd";
-    std::cout.flush();
-    int T;
-    cin >> T;
-    while (T--) {
-    solve();
-    }
-    return 0;
+  int n;
+  cin >> n;
+  cout << n;
+  return 0;
 }
--- 数字接龙.cc
+++ 数字接龙.cc
@@ -1,46 +1,61 @@
-#include <bits/stdc++.h>
-#include <limits>
-#include <vector>
-
+#include <iostream>
+#include <string>
 using namespace std;
-
-int n, k;
-vector<vector<int>> direct = {{-1, 0}, {-1, 1}, {0, 1}, 
-    {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
-
-
-void dfs(vector<vector<int>> &v, int now, vector<int> &res, vector<vector<bool>> visited, int x, int y) {
-    if (res.size() == n * n - 1) {
+const int N=20;
+string ans;
+int g[N][N];//图 
+bool vis[N][N];//访问标记 
+int path[N][N];//记录每个点的移动方向 
+        // 0  1 2 3 4 5 6  7
+int dx[]={-1,-1,0,1,1,1,0,-1};
+        //0 1 2 3 4  5  6  7
+int dy[]={0,1,1,1,0,-1,-1,-1};
+int n,k;//题目输入 
+       //起点      //当前值  //当前组成的字串 
+void dfs(int x,int y,int cur,string s,int dep)//搜索点的个数 
+{
+    if(x==n-1&&y==n-1&&dep==n*n)//搜到了(n-1,n-1)，并且搜过了所有的点 
+    {
+        if(ans.empty()) ans=s;
         return;
     }
-    for (int i = 0; i < direct.size(); ++i) {
-        int nx = x + direct[i][0];
-        int ny = y + direct[i][i];
-        if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-            if (v[nx][ny] == now && !visited[nx][ny]) {
-                if (i == 1) {
-                    if (!visited[nx - 1][ny] && !visited[nx][ny - 1]) {
-                        res.push_back(i);
-                        dfs()
-                    }
-                }
-            }
+    for(int i=0;i<8;i++)//八方向搜索 
+    {
+        int bx=x+dx[i],by=y+dy[i];
+        if(bx<0||bx>n-1||by<0||by>n-1) continue;
+        if(vis[bx][by]) continue;
+        //判交叉
+        if(i==1&&(path[bx][by-1]==3||path[bx+1][by]==7)) continue;
+        else if(i==3&&(path[bx-1][by]==5||path[bx][by-1]==1)) continue;
+        else if(i==5&&(path[bx-1][by]==3||path[bx][by+1]==7)) continue;
+        else if(i==7&&(path[bx+1][by]==1||path[bx][by+1]==5)) continue;
+        //保证是0,1,2…k-1,0
+        if((cur<k-1&&g[bx][by]==cur+1)||(cur==k-1&&g[bx][by]==0))
+        {
+            vis[bx][by]=1;
+            path[x][y]=i;//记录当前点所移动的方向 
+            dfs(bx,by,g[bx][by],s+to_string(i),dep+1);
+            if(!ans.empty()) return;//不空表示已经第一次找到了，不用回溯 
+            vis[bx][by]=0;
+            path[x][y]=-1;//回溯则重置 
         }
     }
 }
-
-
-int main() {
-    cin >> n >> k;
-    vector<vector<int>> v(n, vector<int>(n, 0));
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin >> v[i][j];
+int main()
+{
+    cin>>n>>k;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>g[i][j];
+            path[i][j]=-1;//初始化记录 
         }
     }
-    vector<int> res;
-    vector<vector<bool>> visited(n, vector<bool>(n, false));
-    dfs(v, 0, res, visited, 0, 0);
-
-    return 1;
+    string star;//初始空字符串
+    vis[0][0]=1;//标记(0,0)已经被访问 
+    dfs(0,0,0,star,1);
+    if(!ans.empty()) cout<<ans;
+    else cout<<"-1";
+    return 0; 
 }
