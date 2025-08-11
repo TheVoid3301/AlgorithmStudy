48a848afeabe940840e1d43630ea7e3c375e046a
thevoid3301 <1449812768@qq.com>
2025-08-12 00:59:36 +0800

    经典dp

---
+++ 2787.将一个数字表示成幂的和的方案数.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=2787 lang=cpp
+ *
+ * [2787] 将一个数字表示成幂的和的方案数
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int numberOfWays(int n, int x) {
+        long long mod = 1e9 + 7;
+        vector<vector<long long>> dp(n + 1, vector<long long>(n + 1));
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            long long val = pow(i, x);
+            for (int j = 0; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= val) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - val]) % mod;
+                }
+            }
+        }
+        return dp[n][n];
+    }
+};
+
+// @lc code=end
+
