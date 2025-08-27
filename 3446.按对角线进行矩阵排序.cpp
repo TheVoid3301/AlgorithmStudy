b0c3c598aeb9368ba1f6004ec7d420dad887dd7b
thevoid3301 <1449812768@qq.com>
2025-08-28 01:07:26 +0800

    纯模拟

---
+++ 3446.按对角线进行矩阵排序.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=3446 lang=cpp
+ *
+ * [3446] 按对角线进行矩阵排序
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        for (int i = 0; i < n; i++) {
+            vector<int> tmp;
+            for (int j = 0; i + j < n; j++) {
+                tmp.push_back(grid[i + j][j]);
+            }
+            sort(tmp.begin(), tmp.end(), greater<int>());
+            for (int j = 0; i + j < n; j++) {
+                grid[i + j][j] = tmp[j];
+            }
+        }
+        for (int j = 1; j < n; j++) {
+            vector<int> tmp;
+            for (int i = 0; j + i < n; i++) {
+                tmp.push_back(grid[i][j + i]);
+            }
+            sort(tmp.begin(), tmp.end());
+            for (int i = 0; j + i < n; i++) {
+                grid[i][j + i] = tmp[i];
+            }
+        }
+        return grid;
+    }
+};
+// @lc code=end
+
