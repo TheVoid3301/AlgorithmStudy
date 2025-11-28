2c4e2cbc3344ac17ea815962b212199e436b5692
TheVoid3301 <1449812768@qq.com>
2025-11-28 23:53:41 +0800

    冲刷Hot100

---
+++ leetcode/199. 二叉树的右视图.cc
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+
+        if (!root) {
+            return {};
+        }
+
+        vector<int> res;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            while (size) {
+                TreeNode* t = q.front();
+                q.pop();
+                if (t->left) {
+                    q.push(t->left);
+                }
+                if (t->right) {
+                    q.push(t->right);
+                }
+                size--;
+                if (size == 0) {
+                    res.push_back(t->val);
+                }
+            }
+        }
+
+        return res;
+    }
+};
