245c2e87c971eb80791dab93a6d08fbe2a09eaa4
TheVoid3301 <1449812768@qq.com>
2025-12-25 21:29:29 +0800

    easy

--- leetcode/Counting Leaves.cpp
+++ leetcode/Counting Leaves.cpp
@@ -14,11 +14,47 @@ int main() {
 
     int n, m;
     cin >> n >> m;
-    TreeNode* root = new TreeNode("01");
+    vector<int> tree(n + 1, -1);
     if (n == 0) {
         cout << 0 << " " << 0;
         return 0;
     }
-    
+    for (int i = 0; i < m; ++i) {
+        int id;
+        int childnum;
+        cin >> id >> childnum;
+        for (int j = 0; j < childnum; ++j) {
+            int childid;
+            cin >> childid;
+            tree[childid] = id;
+        }
+    }
+    queue<int> q;
+    q.push(1);
+    int maxDepth = 0;
+    int leafCount = 0;
+    while (!q.empty()) {
+        int size = q.size();
+        for (int i = 0; i < size; ++i) {
+            int node = q.front();
+            q.pop();
+            bool isLeaf = true;
+            for (int j = 1; j <= n; ++j) {
+                if (tree[j] == node) {
+                    q.push(j);
+                    isLeaf = false;
+                }
+            }
+            if (isLeaf) {
+                leafCount++;
+            }
+        }
+        if (q.empty()) {
+            cout << leafCount;
+            break;
+        }
+        cout << leafCount << " ";
+        leafCount = 0;
+    }
     return 0;
 }
